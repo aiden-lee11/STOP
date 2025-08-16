2c0dbdb26e845eb4569ce87e2eaed3386b8eafb3
aiden-lee11 <aidentlee11@gmail.com>
2025-08-16 00:45:36 -0500

    make basic drawing

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
 #include "node/node.h"
 #include <algorithm>
+#include <cstdio>
+#include <ftxui/screen/color.hpp>
 #include <iostream>
 #include <ostream>
 #include <string>
 #include <unordered_set>
 #include <utility>
 #include <vector>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
+#include <thread>   // For sleeping
+#include <chrono>   // For time units
 
 // todo put in test file or smth
 // clang-format off
@@ -28,7 +34,7 @@ const int height = 8;
 const int maxWordLength = (width * height) - (minWordLength * (numWords - 1));
 
 std::unordered_set<std::string> exampleSolution = {
-    "RESIDUE", "VESTIGE", "REMNANT", "LEFTOVERS", "DREGS", "TRACE", "SOUVENIR"};
+	"RESIDUE", "VESTIGE", "REMNANT", "LEFTOVERS", "DREGS", "TRACE", "SOUVENIR"};
 
 std::vector<std::vector<Node>> map(std::vector<std::vector<char>> grid) {
 	std::vector<std::vector<Node>> nodeGrid;
@@ -56,6 +62,42 @@ void debugNodeMap(std::vector<std::vector<Node>> &nodeMap) {
 	for (auto row : nodeMap) {
 		for (auto node : row) {
 			std::cout << node << std::endl;
+		}
+	}
+}
+
+void printBoard(ftxui::Screen& screen, std::vector<std::vector<Node>> &nodeMap) {
+	// auto cell = [](const char* t) { return text(t) | border; };
+	auto cell = [](const Node* t) { 
+		return (
+		ftxui::text(&t->val) | 
+		ftxui::border | 
+		ftxui::color(t->isUsed ? 
+	       ftxui::Color::Red : 
+	       ftxui::Color::White)
+	); 
+	};
+
+	std::vector<ftxui::Elements> lines;
+	for (auto row : nodeMap) {
+		std::vector<ftxui::Element> line;
+		for (auto node : row) {
+			line.push_back(cell(&node));
+		}
+		lines.push_back(line);
+	}
+
+	auto document = ftxui::gridbox({lines});
+	Render(screen, document);
+	screen.Print();
+}
+
+void printSolution(std::vector<std::pair<std::string, std::vector<Node *>>> &words){
+	for (auto word : words) {
+		std::cout << word.first << std::endl;
+
+		for (Node *node : word.second) {
+			std::cout << "\t" << node << std::endl;
 		}
 	}
 }
@@ -116,7 +158,7 @@ dfs(Node *node, std::vector<Node *> &path, const size_t attemptLength) {
 	for (Node *neighbor : node->neighbors) {
 		// is the neighbor even valid
 		if (!neighbor->isUsed &&
-		    std::find(path.begin(), path.end(), neighbor) == path.end()) {
+			std::find(path.begin(), path.end(), neighbor) == path.end()) {
 			auto resPath = dfs(neighbor, path, attemptLength);
 			if (resPath.first.length() != 0) {
 				return resPath;
@@ -129,48 +171,44 @@ dfs(Node *node, std::vector<Node *> &path, const size_t attemptLength) {
 }
 
 std::vector<std::pair<std::string, std::vector<Node *>>>
-solveBoard(std::vector<std::vector<Node>> &nodeMap) {
+solveBoard(std::vector<std::vector<Node>> &nodeMap, size_t attemptLength) {
 	std::vector<std::pair<std::string, std::vector<Node *>>> words;
 
-	for (size_t attemptLength = 4; attemptLength < maxWordLength;
-	     ++attemptLength) {
-		for (auto &row : nodeMap) {
-			for (Node &node : row) {
-				if (node.isUsed) {
-					continue;
-				}
-				// std::cout << "\n[START DFS] From letter '" << node.val
-				//           << "' at (" << node.pos.x << "," << node.pos.y
-				//           << ")\n";
-				std::vector<Node *> path;
-				auto res = dfs(&node, path, attemptLength);
+	for (auto &row : nodeMap) {
+		for (Node &node : row) {
+			if (node.isUsed) {
+				continue;
+			}
+			std::vector<Node *> path;
+			auto res = dfs(&node, path, attemptLength);
 
-				if (res.first.length() != 0) {
-					words.push_back(res);
-				}
+			if (res.first.length() != 0) {
+				words.push_back(res);
 			}
 		}
+	}
+
+	return words;
+} 
+
+
+int main() {
+	std::vector<std::vector<Node>> nodeMap = map(exampleGrid);
+	connect(nodeMap);
+	// debugNodeMap(nodeMap);
+	// printSolution(words);
+	//
+	auto screen = ftxui::Screen::Create(ftxui::Dimension::Full());
+
+	for (size_t attemptLength = 4; attemptLength < maxWordLength;
+	++attemptLength) {
+		std::this_thread::sleep_for(std::chrono::milliseconds(200));
+		auto words = solveBoard(nodeMap, attemptLength);
+		printBoard(screen, nodeMap);
 
 		if (words.size() == numWords) {
 			break;
 		}
 	}
 
-	return words;
-}
-
-int main() {
-	std::vector<std::vector<Node>> nodeMap = map(exampleGrid);
-	connect(nodeMap);
-	// debugNodeMap(nodeMap);
-
-	std::vector<std::pair<std::string, std::vector<Node *>>> words =
-	    solveBoard(nodeMap);
-	for (auto word : words) {
-		std::cout << word.first << std::endl;
-
-		for (Node *node : word.second) {
-			std::cout << "\t" << node << std::endl;
-		}
-	}
 }
